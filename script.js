962841990fe010b6dcdc7e71384f8c6596437f3d
GaidukovaOlesia <lesyagaydukova@gmail.com>
2022-11-04 22:49:52 +0200

    homework-4

---
+++ script.js
@@ -0,0 +1,6 @@
+let firstNumber = parseInt(prompt("Set first number!",''));
+let secondNumber = parseInt(prompt("Set second number!", ''));
+let thirdNumber = parseInt(prompt("Set third number!", ''));
+let send = (firstNumber + secondNumber + thirdNumber)/3;
+alert (`Середнє арифметичне дорівнює:
+ ${send}`);
